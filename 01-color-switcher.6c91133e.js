f1cbe73436bc9824cd25970594bdae125a1dbf63
Oleksandrdz <134951160+Oleksandrdz@users.noreply.github.com>
2023-10-29 07:12:34 +0000

    Deploying to gh-pages from @ Oleksandrdz/goit-js-hw-09@ae6425ae9b197b966167b1e1437e86cd998051bd 🚀

--- 01-color-switcher.6c91133e.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={page:document.querySelector("body"),startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")},n=null;function e(n,e){t.startBtn.disabled=n,t.stopBtn.disabled=e}t.startBtn.addEventListener("click",(function(a){e(!0,!1),n=setInterval((function(){a="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),t.page.style.backgroundColor=a}),1e3)})),t.stopBtn.addEventListener("click",(function(){clearInterval(n),e(!1,!0)}))}();
-//# sourceMappingURL=01-color-switcher.6c91133e.js.map
